60527df7961c17a0237470acd92fd9a371a3ba9a
andrew melis <andrewmelis@gmail.com>
2016-04-16 20:40:55 -0500

    remove stray comments

--- main.go
+++ main.go
@@ -29,17 +29,6 @@ func main() {
 		dstPath := remotePath + file.Name()
 		upload(api, srcPath, dstPath)
 	}
-
-	// fmt.Printf("\nList remote files once...\n")
-	// listRemoteFiles(api, "/nightly-dropbox-sync")
-
-	// fmt.Printf("\nUpload...\n")
-	// upload(api, "./test.txt", "/nightly-dropbox-sync/test.txt")
-	// upload(api, "./test2.txt", "/nightly-dropbox-sync/test2.txt")
-	// upload(api, "./test3.txt", "/nightly-dropbox-sync/test3.txt")
-
-	// fmt.Printf("\nList remote files twice...\n")
-	// listRemoteFiles(api, "/nightly-dropbox-sync")
 }
 
 func listRemoteFiles(client dropbox.Api, path string) {
@@ -65,18 +54,6 @@ func upload(client dropbox.Api, srcPath string, dstPath string) {
 	defer contents.Close()
 
 	commitInfo := files.NewCommitInfo(dstPath)
-
-	//WIP options below
-
-	// commitInfo.ClientModified = time.Now() // FIXME  Error in call to API function "files/upload": HTTP header "Dropbox-API-Arg": client_modified: time data '2016-04-16T17:02:37.15034098-05:00' does not match format '%Y-%m-%dT%H:%M:%SZ'
-
-	// commitInfo.Mode.Tag = "update"
-	// commitInfo.Mode.Update = "49c1a0a8e89e1" // FIXME when figuring out what's new
-	// commitInfo.Autorename = true
-
-	// commitInfo.Mode.Tag = "overwrite"
-	// commitInfo.Autorename = true
-
 	metadata, err := client.Upload(commitInfo, contents)
 	if err != nil {
 		fmt.Printf("✗ Error uploading %s to %s: %+v\n", srcPath, dstPath, err)
